c51e7462999be39d4bdb200bbd6ece8b4dd4b660
Kevin Deganos and Samuel Durfey <samuel@>
2016-04-20 15:49:43 -0700

    Basic set up on find and replace app. File tree and empty class created.

---
+++ src/main/java/Replace.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
+import java.io.Console;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class Replace {
+  public static void main(String[] args) {
+
+  }
+
+  public static String findAndReplace(String phrase, String find, String replace){
+
+  }
+}
---
+++ src/test/java/ReplaceTest.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ReplaceTest {
+
+  // @Test
+  // public void calculateCharacter_returnsScoreForSingleLetterA_1() {
+  //   Replace testReplace = new Replace();
+  //   Integer expected = 1;
+  //   assertEquals(expected, testReplace.calculateCharacter('a'));
+  // }
+}
