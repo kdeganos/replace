787b8127e76016e49d285226dfe388e91e0930ff
Kevin Deganos and Samuel Durfey <kevin@>
2016-04-20 16:40:22 -0700

    findAndReplace now replaces regardless of capitalization. Added a root page "/"

--- src/main/java/Replace.java
+++ src/main/java/Replace.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Arrays;
 import java.io.Console;
+import java.util.regex.Pattern;
 
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -9,10 +10,16 @@ import static spark.Spark.*;
 
 public class Replace {
   public static void main(String[] args) {
-
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/input.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 
   public static String findAndReplace(String phrase, String find, String replace){
-
+    // return phrase.replace(find, replace);
+    return phrase.replaceAll("(?i)"+Pattern.quote(find), replace);
   }
 }
--- src/test/java/ReplaceTest.java
+++ src/test/java/ReplaceTest.java
@@ -5,10 +5,25 @@ import static org.junit.Assert.*;
 
 public class ReplaceTest {
 
-  // @Test
-  // public void calculateCharacter_returnsScoreForSingleLetterA_1() {
-  //   Replace testReplace = new Replace();
-  //   Integer expected = 1;
-  //   assertEquals(expected, testReplace.calculateCharacter('a'));
-  // }
+  @Test
+  public void findAndReplace_replaceHelloWithGoodbye_goodbyeWorld() {
+    Replace testReplace = new Replace();
+    String expected = "goodbye world";
+    assertEquals(expected, testReplace.findAndReplace("hello world", "hello", "goodbye"));
+  }
+
+  @Test
+  public void findAndReplace_replaceHelloWithGoodbyeCaseInsensitive_goodbyeWorld() {
+    Replace testReplace = new Replace();
+    String expected = "goodbye world";
+    assertEquals(expected, testReplace.findAndReplace("Hello world", "hello", "goodbye"));
+  }
+
+    @Test
+    public void findAndReplace_replaceCatwithDog_Doghedral() {
+      Replace testReplace = new Replace();
+      String expected = "I am walking my dog to the doghedral";
+      assertEquals(expected, testReplace.findAndReplace("I am walking my cat to the cathedral", "cat", "dog"));
+
+    }
 }
